9a3cf575865edb1344e6c8ecbb97807a3065fe61
Catalin Petraru <petraru.catalin99@gmail.com>
2022-12-08 14:37:28 +0200

    Refactor the code to use computerScores(), playerScores() and showComputerChoice() functions

--- scripts.js
+++ scripts.js
@@ -46,36 +46,51 @@ function playGame(playerChoice, computerChoice) {
     playerChoice = prompt("Please write down your choice between: rock, paper, scissors");
     // store the computerChoice using the getComputerChoice()
     computerChoice = getComputerChoice();
-    console.log(computerChoice);
     // create an if else if statement to compare the user's answer to the computer's guess
     // if the player's guess matches the computer's guess, it is a draw and ask the user to input another guess via another prompt()
     if (playerChoice == computerChoice) {
+        getComputerChoice(computerChoice);
         alert(`It is a draw. You both chose ${playerChoice}!`);
         playGame();
     }
     // if the player chooses rock and computer chooses paper, the computer wins
     else if (playerChoice == 'rock' && computerChoice == 'paper') {
-        console.log(`Computer wins because ${computerChoice} beats ${playerChoice}`);
+        computerScores(computerChoice, playerChoice);
     }
     // if the player chooses rock and the computer chooses scissors, the player wins
     else if (playerChoice == 'rock' && computerChoice == 'scissors') {
-        console.log(`Player wins because ${playerChoice} beats ${computerChoice}`);
+        playerScores(playerChoice, computerChoice);
     }
     // if the player chooses paper and the computer chooses rock, the player wins
     else if (playerChoice == 'paper' && computerChoice == 'rock') {
-        console.log(`Player wins because ${playerChoice} beats ${computerChoice}`);
+        playerScores(playerChoice, computerChoice);
     }
     // 5) if the player chooses paper and the computer chooses scissors, the computer wins
     else if (playerChoice == 'paper' && computerChoice == 'scissors') {
-        console.log(`Computer wins because ${computerChoice} beats ${playerChoice}`);
+        computerScores(computerChoice, playerChoice)
     }
     // 6) if the player chooses scissors and the computer chooses rock, the computer wins
     else if (playerChoice == 'scissors' && computerChoice == 'rock') {
-        console.log(`Computer wins because ${computerChoice} beats ${playerChoice}`);
+        computerScores(computerChoice, playerChoice);
     }
     // 7) if the player chooses scissors and the computer chooses paper, the player wins
     else if (playerChoice == 'scissors' && computerChoice == 'paper') {
-        console.log(`Player wins because ${playerChoice} beats ${computerChoice}`);
+        playerScores(playerChoice, computerChoice);
     }
+}
 
+function computerScores(computerChoice, playerChoice) {
+    showComputerChoice(computerChoice);
+    console.log(`Computer wins because ${computerChoice} beats ${playerChoice}.`);
+    playGame();
+}
+
+function playerScores(playerChoice, computerChoice) {
+    showComputerChoice(computerChoice);
+    console.log(`Player wins because ${playerChoice} beats ${computerChoice}`);
+    playGame();
+}
+
+function showComputerChoice(computerChoice) {
+    console.log(`The computer chose ${computerChoice}`);
 }
